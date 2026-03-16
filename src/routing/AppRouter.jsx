934e14f50371ae0fd0d0a4eb196cce3e800c6dde
Gerardo <gerardopalonzo@gmail.com>
2026-03-15 22:54:17 -0600

    Fix react router for SPA

--- src/routing/AppRouter.jsx
+++ src/routing/AppRouter.jsx
@@ -1,28 +1,38 @@
-import { BrowserRouter, Navigate, Route } from "react-router-dom"
-import { Home, About, Login, Products } from "../public"
-import { NotFound } from "./NotFound"
-import { AdminGuard } from "./AdminGuard"
-import { AdminRouter } from "./AdminRouter"
-import { GuestGuard } from "./GuestGuard"
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
+import { Home, About, Login, Products } from "../public";
+import { NotFoundPage } from "./NotFoundPage";
+import { AdminGuard } from "./AdminGuard";
+import { AdminRouter } from "./AdminRouter";
+import { GuestGuard } from "./GuestGuard";
 
-export const AppRouter = ({children}) => {
-    return (
-        <BrowserRouter>
-            <NotFound>
-                <Route element={<GuestGuard/>}>
-                    <Route path="/Login" element={<Login />} />
-                </Route>
-                
-                <Route path="/home" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/products" element={<Products />} />
-                <Route path='/' element={<Navigate to='/home' />} />
-                <Route element={<AdminGuard/>}>
-                    <Route path="/private/*" element={<AdminRouter />} />
-                </Route>
-            </NotFound>
-            {children}
-        </BrowserRouter>
-    )
-}
+export const AppRouter = ({ children }) => {
+  return (
+    <HashRouter>
+      <Routes>
+        {/* Rutas públicas con GuestGuard */}
+        <Route element={<GuestGuard />}>
+          <Route path="Login" element={<Login />} />
+        </Route>
 
+        {/* Rutas principales */}
+        <Route path="home" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="products" element={<Products />} />
+
+        {/* Redirigir root a /home */}
+        <Route path="/" element={<Navigate to="home" />} />
+
+        {/* Rutas privadas con AdminGuard */}
+        <Route element={<AdminGuard />}>
+          <Route path="private/*" element={<AdminRouter />} />
+        </Route>
+
+        {/* NotFound */}
+        <Route path="404" element={<NotFoundPage />} />
+        <Route path="*" element={<Navigate to="404" />} />
+      </Routes>
+
+      {children}
+    </HashRouter>
+  );
+};
